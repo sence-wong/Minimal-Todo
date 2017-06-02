811e675acc65fb349e7dec20ced12216f93a2788
sence.wong <huangguang@xunlei.com>
2017-06-02 19:30:11 +0800

    1.编译项目

--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AboutActivity.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/AboutActivity.java
@@ -44,7 +44,7 @@ public class AboutActivity extends AppCompatActivity {
         Intent i = getIntent();
 //        mId = (UUID)i.getSerializableExtra(TodoNotificationService.TODOUUID);
 
-        final Drawable backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        final Drawable backArrow = getResources().getDrawable(R.drawable.ic_alarm_black_18dp);
         if(backArrow!=null){
             backArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
 
--- app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/SettingsActivity.java
+++ app/src/main/java/com/example/avjindersinghsekhon/minimaltodo/SettingsActivity.java
@@ -36,7 +36,7 @@ public class SettingsActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Drawable backArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        final Drawable backArrow = getResources().getDrawable(R.drawable.ic_alarm_black_18dp);
         if(backArrow!=null){
             backArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
 
